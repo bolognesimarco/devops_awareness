34b6b2b560dc3ea2564cd258e69a90272c2de0bc
BOLOGNESI <marco.bolognesi@capgemini.com>
2020-10-16 22:52:16 +0200

    eee

--- complete/src/main/java/com/example/restservice/GreetingController.java
+++ complete/src/main/java/com/example/restservice/GreetingController.java
@@ -9,7 +9,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class GreetingController {
 
-	private static final String template = "Hello, %s!";
+	private static final String TEMPLATE = "Hello, %s!";
 	private final AtomicLong counter = new AtomicLong();
 
 	@GetMapping("/greeting")
@@ -21,6 +21,7 @@ public class GreetingController {
 		int j = 0;
 		while (true) { // Noncompliant; end condition omitted
 		  j++;
+		  if(j==2) break;
 		}
 	}
 }
